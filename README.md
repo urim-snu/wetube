e82fd0c593f48d6df249d4b23e3c2415f2e752b6
snu-urim <woorjb133@gmail.com>
2020-02-27 19:32:59 -0500

    local valuable to pug

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 Cloning Youtube with Vanilla and NodeJS
+
+## Pages:
+
+- [ ] home
+- [ ] Join
+- [ ] Login
+- [x] Search
+- [] User Detail
+- [] Edit Profile
+- [] Change Password
+- [] Upload
+- [] Video Detail
+- [] Edit Video
+
--- controller/videoController.js
+++ controller/videoController.js
@@ -1,5 +1,11 @@
 export const home = (req, res) => res.render("home", {pageTitle: "Home"});
-export const search = (req, res) => res.render("search", {pageTitle: "Search"});
+export const search = (req, res) => {
+    const {
+        query: {term: searchingBy}
+    } = req;
+
+    res.render("search", {pageTitle: "Search", searchingBy})
+};
 export const videos = (req, res) => res.send("videos");
 export const upload = (req, res) => res.send("upload");
 export const videoDetail = (req, res) => res.send("videoDetail");
